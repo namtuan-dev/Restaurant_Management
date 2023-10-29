53d17c79e383e518dee45ee056a925367e6166c5
namtuan-dev <lenamtuan02@gmail.com>
2023-10-29 14:41:24 +0700

    Forgot Password

--- project/src/main/java/com/hcr/swd392g3/project/repository/PersonRepository.java
+++ project/src/main/java/com/hcr/swd392g3/project/repository/PersonRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 	Person findByUserName(String username);
+	Person findByEmail(String email);
 }
